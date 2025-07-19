9189fc0dff1f3de72099c6ab46f31d282e2b2d72
Adam Phillips <adam.phillips@lantanagroup.com>
2025-07-19 14:37:22 -0400

    updated profile service

--- Thetis.Profiles/Application/Services/ProfileService.cs
+++ Thetis.Profiles/Application/Services/ProfileService.cs
@@ -1,3 +1,4 @@
+using LanguageExt.Common;
 using Microsoft.Extensions.Logging;
 using Thetis.Profiles.Data;
 using Thetis.Profiles.Domain;
@@ -8,9 +9,9 @@ internal interface IProfileService
 {
     Task<Profile?> GetProfileByIdAsync(Guid profileId, CancellationToken cancellationToken = default);
     Task<List<Profile>> GetUserProfilesAsync(string sortBy, int pageNumber, int pageSize, CancellationToken cancellationToken = default);
-    Task AddProfileAsync(Profile profile, CancellationToken cancellationToken = default);
-    Task UpdateProfileAsync(Profile profile, CancellationToken cancellationToken = default);
-    Task DeleteProfileAsync(Guid profileId, CancellationToken cancellationToken = default);
+    Task<Result<Profile>> AddProfileAsync(Profile profile, CancellationToken cancellationToken = default);
+    Task<Result<Profile>> UpdateProfileAsync(Profile profile, CancellationToken cancellationToken = default);
+    Task<Result<bool>> DeleteProfileAsync(Guid profileId, CancellationToken cancellationToken = default);
 }
 
 internal class ProfileService(ILogger<ProfileService> logger, IProfileRepository repository) : IProfileService
@@ -27,7 +28,7 @@ internal class ProfileService(ILogger<ProfileService> logger, IProfileRepository
 
         if (profile is null)
         {
-            logger.LogInformation("Profile with ID {ProfileId} not found.", profileId);
+            logger.LogInformation("Profile {ProfileId} not found.", profileId);
         }
         
         return profile;
@@ -41,57 +42,86 @@ internal class ProfileService(ILogger<ProfileService> logger, IProfileRepository
         logger.LogWarning("Invalid pagination parameters: pageNumber={PageNumber}, pageSize={PageSize}", pageNumber, pageSize);
         return [];
     }
-
-    //TODO: implement result patterns for better error handling
-    public Task AddProfileAsync(Profile profile, CancellationToken cancellationToken = default)
+    
+    public  async Task<Result<Profile>> AddProfileAsync(Profile profile, CancellationToken cancellationToken = default)
     {
-        if (profile is null)
-        {
-            logger.LogWarning("Attempted to add a null profile.");
-            throw new ArgumentNullException(nameof(profile), "Profile cannot be null.");
-        }
-
         if (profile.Id == Guid.Empty)
         {
-            profile.Id = Guid.NewGuid(); // Ensure profile has a valid ID
+            profile.Id = Guid.CreateVersion7(); // Ensure profile has a valid ID
         }
 
-        return repository.AddAsync(profile, cancellationToken);
+        try
+        {
+            await repository.AddAsync(profile, cancellationToken);
+            
+            logger.LogInformation("Profile {ProfileId} added successfully.", profile.Id);
+            return new Result<Profile>(profile);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error adding profile {ProfileId}", profile.Id);
+            return new Result<Profile>(ex);
+        }
+        
     }
 
-    public Task UpdateProfileAsync(Profile profile, CancellationToken cancellationToken = default)
+    public async Task<Result<Profile>> UpdateProfileAsync(Profile profile, CancellationToken cancellationToken = default)
     {
-        if (profile is null)
-        {
-            logger.LogWarning("Attempted to update a null profile.");
-            throw new ArgumentNullException(nameof(profile), "Profile cannot be null.");
-        }
-
         if (profile.Id == Guid.Empty)
         {
             logger.LogWarning("Attempted to update a profile with an empty ID.");
-            throw new ArgumentException("Profile ID cannot be empty.", nameof(profile));
+            var ex = new ArgumentException("Profile ID cannot be empty.", nameof(profile));
+            return new Result<Profile>(ex);
         }
 
-        return repository.Update(profile);
+        try
+        {
+            await repository.Update(profile);
+            
+            await repository.SaveChangesAsync(cancellationToken);
+            logger.LogInformation("Profile {ProfileId} updated successfully.", profile.Id);
+            
+            return new Result<Profile>(profile);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error updating profile {ProfileId}", profile.Id);
+            return new Result<Profile>(ex);
+        }
+        
     }
 
-    public async Task DeleteProfileAsync(Guid profileId, CancellationToken cancellationToken = default)
+    public async Task<Result<bool>> DeleteProfileAsync(Guid profileId, CancellationToken cancellationToken = default)
     {
         if (profileId == Guid.Empty)
         {
             logger.LogWarning("Attempted to delete a profile with an empty ID.");
-            throw new ArgumentException("Profile ID cannot be empty.", nameof(profileId));
+            var ex = new ArgumentException("Profile ID cannot be empty.", nameof(profileId));
+            return new Result<bool>(ex);
         }
 
         var profile = await repository.GetByIdAsync(profileId, noTracking: false, cancellationToken);
         if (profile is null)
         {
-            logger.LogInformation("Profile with ID {ProfileId} not found.", profileId);
-            return;
+            logger.LogWarning("Profile with ID {ProfileId} not found.", profileId);
+            var ex = new KeyNotFoundException($"Profile with ID {profileId} not found.");
+            return new Result<bool>(ex);
+        }
+
+        try
+        {
+            await repository.Delete(profile);
+            await repository.SaveChangesAsync(cancellationToken);
+            
+            logger.LogInformation("Profile {ProfileId} deleted successfully.", profileId);
+            return new Result<bool>(true);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error deleting profile {ProfileId}", profileId);
+            return new Result<bool>(ex);
         }
         
-        await repository.Delete(profile);
         
     }
 }
